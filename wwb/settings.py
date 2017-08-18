39cf14af7c4e2bc1774d05695ac6da8daa609dbf
ale <a@414c45.net>
2017-08-18 20:01:20 +0100

    feat: Add basic structure of Django project

---
+++ wwb/settings.py
@@ -0,0 +1,136 @@
+"""
+Django settings for GRRR
+"""
+
+import os
+from django.utils.translation import ugettext_lazy as _
+
+#
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENV_PATH = os.path.abspath( os.path.dirname(__file__) )
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(ENV_PATH, '..', 'static')
+PROJECT_STATIC_FOLDER = 'wwb'
+STATICFILES_DIRS = [
+    ( PROJECT_STATIC_FOLDER, STATIC_ROOT + '/' + PROJECT_STATIC_FOLDER + '/' ),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(ENV_PATH, '..', 'media')
+MAINTENANCE_IGNORE_URLS = (
+    r'^/admin/.*',
+    r'^/login$',
+)
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = '/logout'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_OPEN = True
+REGISTRATION_AUTO_LOGIN = True
+
+# Name of site in the document title
+DOCUMENT_TITLE = 'WWB S. Coop. And.'
+DOCUMENT_DESCRIPTION = _('Cooperativa andaluza de servicios tecnológicos basados en tecnologías libres.')
+
+# Sites ID
+SITE_ID = 1
+
+#
+# Application definition
+#
+
+CONTRIB_APPS = [
+    'django.contrib.admin',
+    'django.contrib.sites',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+PROJECT_APPS = [
+
+]
+
+INSTALLED_APPS = CONTRIB_APPS + PROJECT_APPS
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'wwb.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [ 'templates' ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'apps.users.context_processors.unseen_messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'wwb.wsgi.application'
+
+# Password validation
+# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/dev/topics/i18n/
+
+LANGUAGE_CODE = 'es'
+LANGUAGES = (
+    ('es', _('Español')),
+    ('en', _('Inglés')),
+)
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+#
+# Import private settings
+#
+from .private_settings import *
