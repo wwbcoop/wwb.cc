fe300e3bb28c6535c686624830afebf1c1f19c75
ale <a@414c45.net>
2017-10-24 19:32:24 +0100

    feat: Update main urls

--- wwb/urls.py
+++ wwb/urls.py
@@ -2,11 +2,27 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+# project
+from django.conf import settings
+from apps.views import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^i18n/',  include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
-    url(r'^', include('apps.models.urls', namespace='modelforms')),
+    url(r'^$', views.FrontView.as_view(), name="front"),
+    url(r'^',  include('apps.models.urls', namespace='modelforms')),
+    # Registration urls
+    url(r'', include('registration.backends.default.urls')),
+    # Contact form
+    url(r'^contacta$', ContactFormView.as_view(form_class=AkismetContactForm), name='contact_form'),
+    url(r'^contacta/', include('contact_form.urls')),
+
 )
+
+if settings.DEBUG == True:
+   urlpatterns += static( settings.STATIC_URL, document_root = settings.STATIC_ROOT )
+   urlpatterns += static( settings.MEDIA_URL,  document_root = settings.MEDIA_ROOT )
